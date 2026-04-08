283d664cb0c19110f845874d2d904bda6a0ad886
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-04-08 20:02:50 +0530

    Update cloudinaryConfig.js

--- backend/Middleware/cloudinaryConfig.js
+++ backend/Middleware/cloudinaryConfig.js
@@ -4,6 +4,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 // Apne Cloudinary Dashboard se credentials yahan daalein
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY,    
