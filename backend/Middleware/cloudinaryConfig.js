c10fbd84a62f3a73c4e88d55bf86973820c0bd10
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-04-08 18:09:56 +0530

    Create cloudinaryConfig.js

---
+++ backend/Middleware/cloudinaryConfig.js
@@ -0,0 +1,22 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+// Apne Cloudinary Dashboard se credentials yahan daalein
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY,    
+  api_secret: process.env.CLOUDINARY_API_SECRET   
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'chat_app_dps', // Cloudinary par folder ka naam
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = { upload, cloudinary };
